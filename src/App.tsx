7ac7bd332e10c118ff3c9889f3122452207312ae
Brandon <bdonbson@udel.edu>
2023-04-03 14:31:17 -0400

    Removed older files for the class project that were not needed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
+import { Character } from "./components/Character";
+import { CHARACTERS } from "./data/characters.json";
+import { charwheelglid } from "./interfaces/charwheelglid";
+import { dataFormat } from "./interfaces/dataFormat";
 
 function App(): JSX.Element {
+    const characterData: charwheelglid[] = CHARACTERS as charwheelglid[]; //all of the character data
     return (
         <div className="App">
             <header className="App-header">Testing Grounds</header>
+            <Character
+                name="Roy"
+                picture="./pictures/charactericons/MK8D_Roy.png"
+                data={characterData[0].data}
+            ></Character>
         </div>
     );
 }
--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -4,7 +4,8 @@ import { charwheelglid } from "../interfaces/charwheelglid";
 export function Character(char: charwheelglid): JSX.Element {
     return (
         <div>
-            {char.name} has source image {char.picture} and data {char.data}
+            {char.name} has source image <img src={char.picture}></img> and data
+            {char.data}
         </div>
     );
 }
