21e7e93277aafeebfc27999ccd728e47b67fe446
Brandon <bdonbson@udel.edu>
2023-04-03 14:58:25 -0400

    Redid interfaces and renamed them, need to adjust JSONs to use these new interfaces.

--- src/interfaces/charwheelglid.ts
+++
@@ -1,6 +0,0 @@
-import { dataFormat } from "./dataFormat";
-export interface charwheelglid {
-    name: string;
-    picture: string;
-    data: dataFormat;
-}
--- src/interfaces/dataFormat.ts
+++ src/interfaces/dataFormat.ts
@@ -1,15 +1,10 @@
+import { speedAndHandling } from "./speedAndHandling";
 export interface dataFormat {
-    wg: number; //weight
-    ac: number; //acceleration
-    on: number; //on-raod traction
-    of: number; //off-road traction
-    mt: number; //mini-turbo
-    sl: number; //ground speed
-    sw: number; //water speed
-    sa: number; //anti-gravity speed
-    sg: number; //air speed
-    tl: number; //ground handling
-    tw: number; //water handling
-    ta: number; //anti-gravity handling
-    tg: number; //air handling;
+    speed: speedAndHandling;
+    acceleration: number;
+    weight: number;
+    handling: speedAndHandling;
+    traction: number;
+    miniTurbo: number;
+    invincibilty: number;
 }
